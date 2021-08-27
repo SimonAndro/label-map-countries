bc72e92221f081c5ae43803e0493e5aea5f4b43b
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-08-28 03:47:07 +0800

    move mouse to label countries

---
+++ main.js
@@ -0,0 +1,343 @@
+let countries = [{
+        a2: "sm_state_DZ",
+        countryname: "ALGERIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_AO",
+        countryname: "ANGOLA",
+        state: 0
+    },
+    {
+        a2: "sm_state_BJ",
+        countryname: "BENIN",
+        state: 0
+    },
+    {
+        a2: "sm_state_BW",
+        countryname: "BOTSWANA",
+        state: 0
+    },
+    {
+        a2: "sm_state_BF",
+        countryname: "BURKINA FASO",
+        state: 0
+    },
+    {
+        a2: "sm_state_BI",
+        countryname: "BURUNDI",
+        state: 0
+    },
+    {
+        a2: "sm_state_CM",
+        countryname: "CAMEROON",
+        state: 0
+    },
+    {
+        a2: "sm_state_CV",
+        countryname: "CAPE VERDE",
+        state: 0
+    },
+    {
+        a2: "sm_state_CF",
+        countryname: "CENTRAL AFRICAN REPUBLIC",
+        state: 0
+    },
+    {
+        a2: "sm_state_TD",
+        countryname: "CHAD",
+        state: 0
+    },
+    {
+        a2: "sm_state_KM",
+        countryname: "COMOROS",
+        state: 0
+    },
+    {
+        a2: "sm_state_CG",
+        countryname: "CONGO",
+        state: 0
+    },
+    {
+        a2: "sm_state_CD",
+        countryname: "CONGO, DRC",
+        state: 0
+    },
+    {
+        a2: "sm_state_CI",
+        countryname: "COTE D’IVOIRE",
+        state: 0
+    },
+    {
+        a2: "sm_state_DJ",
+        countryname: "DJIBOUTI",
+        state: 0
+    },
+    {
+        a2: "sm_state_EG",
+        countryname: "EGYPT",
+        state: 0
+    },
+    {
+        a2: "sm_state_GQ",
+        countryname: "EQUATORIAL GUINEA",
+        state: 0
+    },
+    {
+        a2: "sm_state_ER",
+        countryname: "ERITREA",
+        state: 0
+    },
+    {
+        a2: "sm_state_ET",
+        countryname: "ETHIOPIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_GA",
+        countryname: "GABON",
+        state: 0
+    },
+    {
+        a2: "sm_state_GM",
+        countryname: "GAMBIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_GH",
+        countryname: "GHANA",
+        state: 0
+    },
+    {
+        a2: "sm_state_GN",
+        countryname: "GUINEA",
+        state: 0
+    },
+    {
+        a2: "sm_state_GW",
+        countryname: "GUINEA-BISSAU",
+        state: 0
+    },
+    {
+        a2: "sm_state_KE",
+        countryname: "KENYA",
+        state: 0
+    },
+    {
+        a2: "sm_state_LS",
+        countryname: "LESOTHO",
+        state: 0
+    },
+    {
+        a2: "sm_state_LR",
+        countryname: "LIBERIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_LY",
+        countryname: "LIBYAN ARAB JAMAHIRIYA",
+        state: 0
+    },
+    {
+        a2: "sm_state_MG",
+        countryname: "MADAGASCAR",
+        state: 0
+    },
+    {
+        a2: "sm_state_ML",
+        countryname: "MALI",
+        state: 0
+    },
+    {
+        a2: "sm_state_MW",
+        countryname: "MALAWI",
+        state: 0
+    },
+    {
+        a2: "sm_state_MR",
+        countryname: "MAURITANIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_MU",
+        countryname: "MAURITIUS",
+        state: 0
+    },
+    {
+        a2: "sm_state_YT",
+        countryname: "MAYOTTE",
+        state: 0
+    },
+    {
+        a2: "sm_state_MA",
+        countryname: "MOROCCO",
+        state: 0
+    },
+    {
+        a2: "sm_state_MZ",
+        countryname: "MOZAMBIQUE",
+        state: 0
+    },
+    {
+        a2: "sm_state_NA",
+        countryname: "NAMIBIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_NE",
+        countryname: "NIGER",
+        state: 0
+    },
+    {
+        a2: "sm_state_NG",
+        countryname: "NIGERIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_RE",
+        countryname: "REUNION ISLAND",
+        state: 0
+    },
+    {
+        a2: "sm_state_RW",
+        countryname: "RWANDA",
+        state: 0
+    },
+    {
+        a2: "sm_state_ST",
+        countryname: "SAO TOME AND PRINCIPE",
+        state: 0
+    },
+    {
+        a2: "sm_state_SN",
+        countryname: "SENEGAL",
+        state: 0
+    },
+    {
+        a2: "sm_state_SC",
+        countryname: "SEYCHELLES",
+        state: 0
+    },
+    {
+        a2: "sm_state_SL",
+        countryname: "SIERRA LEONE",
+        state: 0
+    },
+    {
+        a2: "sm_state_SO",
+        countryname: "SOMALIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_ZA",
+        countryname: "SOUTH AFRICA",
+        state: 0
+    },
+    {
+        a2: "sm_state_SS",
+        countryname: "SOUTH SUDAN",
+        state: 0
+    },
+    {
+        a2: "sm_state_SD",
+        countryname: "SUDAN",
+        state: 0
+    },
+    {
+        a2: "sm_state_SZ",
+        countryname: "SWAZILAND",
+        state: 0
+    },
+    {
+        a2: "sm_state_TZ",
+        countryname: "TANZANIA, UNITED REPUBLIC OF",
+        state: 0
+    },
+    {
+        a2: "sm_state_TG",
+        countryname: "TOGO",
+        state: 0
+    },
+    {
+        a2: "sm_state_TN",
+        countryname: "TUNISIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_UG",
+        countryname: "UGANDA",
+        state: 0
+    },
+    {
+        a2: "sm_state_EH",
+        countryname: "WESTERN SAHARA",
+        state: 0
+    },
+    {
+        a2: "sm_state_ZM",
+        countryname: "ZAMBIA",
+        state: 0
+    },
+    {
+        a2: "sm_state_ZW",
+        countryname: "ZIMBABWE",
+        state: 0
+
+    }
+];
+
+var countryIndex = 0;
+$(document).ready(function () {
+    const onMouseMove = (e) => {
+        $("#country-at-cursor").css({
+            "left": e.pageX + 10 + 'px',
+            "top": e.pageY + 'px'
+        });
+    }
+    document.addEventListener('mousemove', onMouseMove);
+
+    countries = shuffle(countries);
+
+    $("#country-at-cursor").html(countries[countryIndex].countryname);
+
+    $("#map-area").on('mouseup', function (e) {
+        var target = $(e.target).attr("class");
+
+        var option = countries[countryIndex].a2;
+        console.log(target,option);
+        if (target == option) {
+            $("." + option).css({
+                "fill": "#c0ffee"
+            });
+        } else {
+            $("." + option).css({
+                "fill": "#F00"
+            });
+        }
+        if(countryIndex < countries.length){
+            $("#country-at-cursor").html(countries[++countryIndex].countryname);
+        }else{
+            $("#country-at-cursor").html("");
+        }
+    });
+});
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
+    }
+
+    return array;
+}
